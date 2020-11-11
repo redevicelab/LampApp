41457b29008224a391ab2d0712a9a8d0497753fb
Redevice <redevice7@gmail.com>
2020-11-11 16:24:41 +0600

    Добавьте файлы проекта.

---
+++ LampApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LampApp.ViewModels
+{
+    public class MainViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _Title = "LampApp";
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            //set 
+            //{
+            //    if (Equals(_Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged("Title");
+            //}
+            set => Set(ref _Title, value, "Title");
+        }
+        #endregion
+    }
+}
