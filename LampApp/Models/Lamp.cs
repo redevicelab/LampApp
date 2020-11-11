8abbd52c45a77104d520216607c5d4087b36d6b4
Redevice <redevice7@gmail.com>
2020-11-12 01:00:32 +0600

    Model and ViewModel

---
+++ LampApp/Models/Lamp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using LampApp.ViewModels;
+
+namespace LampApp.Models
+{
+    public class Lamp : ViewModel
+    {
+        private string _Address;
+        private int _Port;
+        private bool _Power;
+
+        public string Address
+        {
+            get => _Address;
+            set => Set(ref _Address, value, "Status");
+        }
+
+        public int Port
+        {
+            get => _Port;
+            set => Set(ref _Port, value, "Status");
+        }
+
+        public bool Power
+        {
+            get => Power;
+            set => _Power = value;
+        }
+
+        /// <summary>
+        /// Метод получения натроек от лампы
+        /// </summary>
+        public void GetSettings()
+        {
+
+        }
+        /// <summary>
+        /// Метод отправки сообщения на лампу
+        /// </summary>
+        /// <param name="settingName">Название настроки</param>
+        /// <param name="value">Значение</param>
+        public void SendSetting(string settingName, int value)
+        {
+
+        }
+
+        public void PowerOnOff()
+        {
+
+        }
+    }
+}
--- LampApp/ViewModels/MainViewModel.cs
+++ LampApp/ViewModels/MainViewModel.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using LampApp.Commands;
+using LampApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace LampApp.ViewModels
 {
     public class MainViewModel : ViewModel
     {
+        private Lamp lamp = new Lamp();
+
         #region Заголовок окна
         private string _Title = "LampApp";
         /// <summary>Заголовок окна</summary>
@@ -24,7 +29,7 @@ namespace LampApp.ViewModels
         }
         #endregion
 
-        #region Status : status - Статус программы
+        #region Статус программы
 
         private string _Status;
         /// <summary>Статус программы</summary>
@@ -35,6 +40,86 @@ namespace LampApp.ViewModels
         }
 
 
+        #endregion
+
+        #region Слайдер скрость
+        private int _Speed;
+        /// <summary>
+        /// Слайдер скорость
+        /// </summary>
+        public int Speed
+        {
+            get => _Speed;
+            set
+            {
+                Set(ref _Speed, value, "Status");
+                lamp.SendSetting("SPD", _Speed);
+            }
+        }
+        #endregion
+
+        #region Слайдер масштаб
+        private int _Scale;
+        /// <summary>
+        /// Слайдер Масштаб
+        /// </summary>
+        public int Scale
+        {
+            get => _Scale;
+            set
+            {
+                Set(ref _Scale, value, "Status");
+                lamp.SendSetting("SCA", _Scale);
+            }
+        }
+        #endregion
+
+        #region Слайдер яркость
+        private int _Brightness;
+        /// <summary>
+        /// Слайдер Яркость
+        /// </summary>
+        public int Brightness
+        {
+            get => _Brightness;
+            set
+            {
+                Set(ref _Brightness, value, "Status");
+                lamp.SendSetting("BRI", _Scale);
+            }
+        }
+        #endregion
+
+        #region Команда получение настроек
+        /// <summary>
+        /// Команда на получение настроек от лампы
+        /// </summary>
+        public ICommand GetSettings
+        {
+            get
+            {
+                return new DelegateCommand((obj) =>
+                {
+                    lamp.GetSettings();
+                });
+            }
+        }
+        #endregion
+
+        #region Команда получение настроек
+        /// <summary>
+        /// Команда на вкл/откл лампы
+        /// </summary>
+        public ICommand Power
+        {
+            get
+            {
+                return new DelegateCommand((obj) =>
+                {
+                    lamp.PowerOnOff();
+                });
+            }
+        }
         #endregion
     }
 }
